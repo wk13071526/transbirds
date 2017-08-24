5bff50903aeb6a60312e8ae582b8fadc4f732c2e
“wk13071526” <wk13071526@163.com>
2017-08-24 05:08:27 +0000

    start app

--- transbirds/settings.py
+++ transbirds/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ')%#$#2_9+ke0pcl27uoz7z*-otj4s3^)k(g%$+-b*%*lx^_9n('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
